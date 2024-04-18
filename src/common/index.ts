6e0b0a5a98fbf1f668d1408186a046f991e1d491
Edward Thang <edward.thang@admenergy.com>
2024-04-18 13:16:03 -0700

    Added getMime

--- src/common/index.ts
+++ src/common/index.ts
@@ -5,6 +5,7 @@ export * from "./bestConversionHelper";
 export * from "./bestTimeUnitMS";
 export * from "./createPromise";
 export * from "./getIn";
+export * from "./getMime";
 export * from "./removeDiacritics";
 export * from "./sanitizeStringDisplay";
 export * from "./sanitizeStringKey";
