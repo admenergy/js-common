febe604ca31e4b9ccb97ea6562e62c74f753d248
Edward Thang <project@nyaarium.com>
2025-03-18 17:24:53 -0700

    Fixing MUI

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,14 @@
+import { Box, Card, CardContent, CardHeader, Typography } from "@mui/material";
+
 export default function PageIndex() {
   return (
-    <>
-      <div>
-        <b>Hi. :)</b>
-      </div>
-    </>
+    <Box m={2}>
+      <Card>
+        <CardHeader title="Hi. :)" />
+        <CardContent>
+          <Typography>This is a test of the card component.</Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
