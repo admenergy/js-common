8830d1393c6419afdbf86bb3bf46ab3f0e3bb8e6
Edward Thang <project@nyaarium.com>
2025-12-12 17:14:58 -0800

    Build

--- dist/js/common/index.js
+++ dist/js/common/index.js
@@ -12,7 +12,134 @@ return /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 85:
+/***/ 202:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   bestTimeUnitMS: () => (/* binding */ bestTimeUnitMS)
+/* harmony export */ });
+/* harmony import */ var _bestConversionHelper__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(281);
+
+
+/**
+ * Convert a millisecond number to human readable units.
+ *
+ * @param ms - Value to convert.
+ *
+ * @returns An instance of ConversionResult with value, round, unit, and a toString method that flattens the output.
+ *
+ * @throws TypeError if the parameter types are incorrect.
+ *
+ * @example
+ * bestTimeUnitMS(4500000);
+ * console.log(result.toString());
+ * -> "1.25 h"
+ */
+function bestTimeUnitMS(ms) {
+  if (typeof ms !== "number") {
+    throw new TypeError("bestTimeUnitMS(ms) : 'ms' must be a number.");
+  }
+  var conversions = [{
+    unit: "μs",
+    value: 1 / 1000
+  }, {
+    unit: "ms",
+    value: 1
+  }, {
+    unit: "s",
+    value: 1 * 1000
+  }, {
+    unit: "m",
+    value: 1 * 1000 * 60
+  }, {
+    unit: "h",
+    value: 1 * 1000 * 60 * 60
+  }, {
+    unit: "d",
+    value: 1 * 1000 * 60 * 60 * 24
+  }];
+  var conversion = (0,_bestConversionHelper__WEBPACK_IMPORTED_MODULE_0__.bestConversionHelper)(ms, 1.2, conversions, 1);
+  var value = ms / conversion.value;
+  var round = Math.round(value * 100) / 100;
+  return new _bestConversionHelper__WEBPACK_IMPORTED_MODULE_0__.ConversionResult(value, round, conversion.unit);
+}
+
+/***/ }),
+
+/***/ 281:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   ConversionResult: () => (/* binding */ ConversionResult),
+/* harmony export */   bestConversionHelper: () => (/* binding */ bestConversionHelper)
+/* harmony export */ });
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
+function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
+var ConversionResult = /*#__PURE__*/_createClass(function ConversionResult(value, round, unit) {
+  _classCallCheck(this, ConversionResult);
+  this.value = value;
+  this.round = round;
+  this.unit = unit;
+
+  // Define the toString method as non-enumerable
+  Object.defineProperty(this, "toString", {
+    value: function value() {
+      return "".concat(this.round, " ").concat(this.unit);
+    },
+    enumerable: false
+  });
+});
+
+/**
+ * Best Conversion Helper
+ *
+ * Helper to convert to human readable units
+ *
+ * @param startingNumber - Value to convert.
+ * @param threshold - Multiplier before converting to the next unit (recommended: 1.2).
+ * @param conversions - Array of conversions.
+ * @param startingConversionsIndex - Which index in `conversions` represents `startingNumber`.
+ *
+ * @returns The best conversion in the conversion table.
+ *
+ * @throws TypeError if the parameter types are bad.
+ *
+ * @example
+ * See bestByteUnit.js and bestTimeUnitMS.js
+ */
+function bestConversionHelper(startingNumber, threshold, conversions, startingConversionsIndex) {
+  if (typeof startingNumber !== "number") {
+    throw new TypeError("bestConversionHelper(startingNumber, threshold, conversions, startingConversionsIndex) : 'startingNumber' must be a number.");
+  }
+  if (typeof threshold !== "number") {
+    throw new TypeError("bestConversionHelper(startingNumber, threshold, conversions, startingConversionsIndex) : 'threshold' must be a number.");
+  }
+  if (!Array.isArray(conversions)) {
+    throw new TypeError("bestConversionHelper(startingNumber, threshold, conversions, startingConversionsIndex) : 'conversions' must be an array.");
+  }
+  if (typeof startingConversionsIndex !== "number") {
+    throw new TypeError("bestConversionHelper(startingNumber, threshold, conversions, startingConversionsIndex) : 'startingConversionsIndex' must be a number.");
+  }
+  startingNumber = Math.abs(startingNumber);
+  var i = startingConversionsIndex;
+  while (0 < i && startingNumber <= conversions[i - 1].value * threshold) {
+    i--;
+  }
+  while (i < conversions.length - 1 && conversions[i + 1].value * threshold <= startingNumber) {
+    i++;
+  }
+  return conversions[i];
+}
+
+/***/ }),
+
+/***/ 549:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
@@ -105,14 +232,14 @@ var NotFoundError = /*#__PURE__*/function (_Error3) {
 
 /***/ }),
 
-/***/ 550:
+/***/ 718:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   createPromise: () => (/* binding */ createPromise)
 /* harmony export */ });
-/* harmony import */ var _common_bestTimeUnitMS__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(738);
+/* harmony import */ var _common_bestTimeUnitMS__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(202);
 
 /**
  * Promise Helper
@@ -167,133 +294,6 @@ function createPromise() {
     resolve: resolve,
     reject: reject
   };
-}
-
-/***/ }),
-
-/***/ 738:
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   bestTimeUnitMS: () => (/* binding */ bestTimeUnitMS)
-/* harmony export */ });
-/* harmony import */ var _bestConversionHelper__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(777);
-
-
-/**
- * Convert a millisecond number to human readable units.
- *
- * @param ms - Value to convert.
- *
- * @returns An instance of ConversionResult with value, round, unit, and a toString method that flattens the output.
- *
- * @throws TypeError if the parameter types are incorrect.
- *
- * @example
- * bestTimeUnitMS(4500000);
- * console.log(result.toString());
- * -> "1.25 h"
- */
-function bestTimeUnitMS(ms) {
-  if (typeof ms !== "number") {
-    throw new TypeError("bestTimeUnitMS(ms) : 'ms' must be a number.");
-  }
-  var conversions = [{
-    unit: "μs",
-    value: 1 / 1000
-  }, {
-    unit: "ms",
-    value: 1
-  }, {
-    unit: "s",
-    value: 1 * 1000
-  }, {
-    unit: "m",
-    value: 1 * 1000 * 60
-  }, {
-    unit: "h",
-    value: 1 * 1000 * 60 * 60
-  }, {
-    unit: "d",
-    value: 1 * 1000 * 60 * 60 * 24
-  }];
-  var conversion = (0,_bestConversionHelper__WEBPACK_IMPORTED_MODULE_0__.bestConversionHelper)(ms, 1.2, conversions, 1);
-  var value = ms / conversion.value;
-  var round = Math.round(value * 100) / 100;
-  return new _bestConversionHelper__WEBPACK_IMPORTED_MODULE_0__.ConversionResult(value, round, conversion.unit);
-}
-
-/***/ }),
-
-/***/ 777:
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   ConversionResult: () => (/* binding */ ConversionResult),
-/* harmony export */   bestConversionHelper: () => (/* binding */ bestConversionHelper)
-/* harmony export */ });
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
-function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
-function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
-var ConversionResult = /*#__PURE__*/_createClass(function ConversionResult(value, round, unit) {
-  _classCallCheck(this, ConversionResult);
-  this.value = value;
-  this.round = round;
-  this.unit = unit;
-
-  // Define the toString method as non-enumerable
-  Object.defineProperty(this, "toString", {
-    value: function value() {
-      return "".concat(this.round, " ").concat(this.unit);
-    },
-    enumerable: false
-  });
-});
-
-/**
- * Best Conversion Helper
- *
- * Helper to convert to human readable units
- *
- * @param startingNumber - Value to convert.
- * @param threshold - Multiplier before converting to the next unit (recommended: 1.2).
- * @param conversions - Array of conversions.
- * @param startingConversionsIndex - Which index in `conversions` represents `startingNumber`.
- *
- * @returns The best conversion in the conversion table.
- *
- * @throws TypeError if the parameter types are bad.
- *
- * @example
- * See bestByteUnit.js and bestTimeUnitMS.js
- */
-function bestConversionHelper(startingNumber, threshold, conversions, startingConversionsIndex) {
-  if (typeof startingNumber !== "number") {
-    throw new TypeError("bestConversionHelper(startingNumber, threshold, conversions, startingConversionsIndex) : 'startingNumber' must be a number.");
-  }
-  if (typeof threshold !== "number") {
-    throw new TypeError("bestConversionHelper(startingNumber, threshold, conversions, startingConversionsIndex) : 'threshold' must be a number.");
-  }
-  if (!Array.isArray(conversions)) {
-    throw new TypeError("bestConversionHelper(startingNumber, threshold, conversions, startingConversionsIndex) : 'conversions' must be an array.");
-  }
-  if (typeof startingConversionsIndex !== "number") {
-    throw new TypeError("bestConversionHelper(startingNumber, threshold, conversions, startingConversionsIndex) : 'startingConversionsIndex' must be a number.");
-  }
-  startingNumber = Math.abs(startingNumber);
-  var i = startingConversionsIndex;
-  while (0 < i && startingNumber <= conversions[i - 1].value * threshold) {
-    i--;
-  }
-  while (i < conversions.length - 1 && conversions[i + 1].value * threshold <= startingNumber) {
-    i++;
-  }
-  return conversions[i];
 }
 
 /***/ })
@@ -383,9 +383,9 @@ __webpack_require__.d(__webpack_exports__, {
 });
 
 // EXTERNAL MODULE: ./app/ErrorTypes/index.ts
-var ErrorTypes = __webpack_require__(85);
+var ErrorTypes = __webpack_require__(549);
 // EXTERNAL MODULE: ./app/common/bestConversionHelper.ts
-var bestConversionHelper = __webpack_require__(777);
+var bestConversionHelper = __webpack_require__(281);
 ;// ./app/common/benchmark.ts
 function _regenerator() { /*! regenerator-runtime -- Copyright (c) 2014-present, Facebook, Inc. -- license (MIT): https://github.com/babel/babel/blob/main/packages/babel-helpers/LICENSE */ var e, t, r = "function" == typeof Symbol ? Symbol : {}, n = r.iterator || "@@iterator", o = r.toStringTag || "@@toStringTag"; function i(r, n, o, i) { var c = n && n.prototype instanceof Generator ? n : Generator, u = Object.create(c.prototype); return _regeneratorDefine2(u, "_invoke", function (r, n, o) { var i, c, u, f = 0, p = o || [], y = !1, G = { p: 0, n: 0, v: e, a: d, f: d.bind(e, 4), d: function d(t, r) { return i = t, c = 0, u = e, G.n = r, a; } }; function d(r, n) { for (c = r, u = n, t = 0; !y && f && !o && t < p.length; t++) { var o, i = p[t], d = G.p, l = i[2]; r > 3 ? (o = l === n) && (u = i[(c = i[4]) ? 5 : (c = 3, 3)], i[4] = i[5] = e) : i[0] <= d && ((o = r < 2 && d < i[1]) ? (c = 0, G.v = n, G.n = i[1]) : d < l && (o = r < 3 || i[0] > n || n > l) && (i[4] = r, i[5] = n, G.n = l, c = 0)); } if (o || r > 1) return a; throw y = !0, n; } return function (o, p, l) { if (f > 1) throw TypeError("Generator is already running"); for (y && 1 === p && d(p, l), c = p, u = l; (t = c < 2 ? e : u) || !y;) { i || (c ? c < 3 ? (c > 1 && (G.n = -1), d(c, u)) : G.n = u : G.v = u); try { if (f = 2, i) { if (c || (o = "next"), t = i[o]) { if (!(t = t.call(i, u))) throw TypeError("iterator result is not an object"); if (!t.done) return t; u = t.value, c < 2 && (c = 0); } else 1 === c && (t = i["return"]) && t.call(i), c < 2 && (u = TypeError("The iterator does not provide a '" + o + "' method"), c = 1); i = e; } else if ((t = (y = G.n < 0) ? u : r.call(n, G)) !== a) break; } catch (t) { i = e, c = 1, u = t; } finally { f = 1; } } return { value: t, done: y }; }; }(r, o, i), !0), u; } var a = {}; function Generator() {} function GeneratorFunction() {} function GeneratorFunctionPrototype() {} t = Object.getPrototypeOf; var c = [][n] ? t(t([][n]())) : (_regeneratorDefine2(t = {}, n, function () { return this; }), t), u = GeneratorFunctionPrototype.prototype = Generator.prototype = Object.create(c); function f(e) { return Object.setPrototypeOf ? Object.setPrototypeOf(e, GeneratorFunctionPrototype) : (e.__proto__ = GeneratorFunctionPrototype, _regeneratorDefine2(e, o, "GeneratorFunction")), e.prototype = Object.create(u), e; } return GeneratorFunction.prototype = GeneratorFunctionPrototype, _regeneratorDefine2(u, "constructor", GeneratorFunctionPrototype), _regeneratorDefine2(GeneratorFunctionPrototype, "constructor", GeneratorFunction), GeneratorFunction.displayName = "GeneratorFunction", _regeneratorDefine2(GeneratorFunctionPrototype, o, "GeneratorFunction"), _regeneratorDefine2(u), _regeneratorDefine2(u, o, "Generator"), _regeneratorDefine2(u, n, function () { return this; }), _regeneratorDefine2(u, "toString", function () { return "[object Generator]"; }), (_regenerator = function _regenerator() { return { w: i, m: f }; })(); }
 function _regeneratorDefine2(e, r, n, t) { var i = Object.defineProperty; try { i({}, "", {}); } catch (e) { i = 0; } _regeneratorDefine2 = function _regeneratorDefine(e, r, n, t) { function o(r, n) { _regeneratorDefine2(e, r, function (e) { return this._invoke(r, n, e); }); } r ? i ? i(e, r, { value: n, enumerable: !t, configurable: !t, writable: !t }) : e[r] = n : (o("next", 0), o("throw", 1), o("return", 2)); }, _regeneratorDefine2(e, r, n, t); }
@@ -544,9 +544,9 @@ function bestByteUnit(_byte) {
   return new bestConversionHelper.ConversionResult(value, round, conversion.unit);
 }
 // EXTERNAL MODULE: ./app/common/bestTimeUnitMS.ts
-var bestTimeUnitMS = __webpack_require__(738);
+var bestTimeUnitMS = __webpack_require__(202);
 // EXTERNAL MODULE: ./app/common/createPromise.ts
-var createPromise = __webpack_require__(550);
+var createPromise = __webpack_require__(718);
 ;// ./app/common/EventDispatcher.ts
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
@@ -1196,6 +1196,7 @@ var defaultDiacriticsRemovalMap = [
   letters: /\r\n/g
 },
 // Unicode spaces and tabs
+// eslint-disable-next-line no-control-regex
 {
   base: " ",
   letters: /[\u0009\u00A0\u2000-\u200A\u202F\u205F\u3000]/g
@@ -1219,6 +1220,7 @@ var defaultDiacriticsRemovalMap = [
   letters: /[\u005F\uFE4D-\uFE4F]/g
 },
 // Invisible formatting characters
+// eslint-disable-next-line no-control-regex
 {
   base: "",
   letters: /[\u0002\u0003]/g
